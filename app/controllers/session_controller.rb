51920f26bcdd2bd688739a1492ece9942c1977fb
Gui Carneiro <gui.carneiro15@gmail.com>
2014-11-06 17:42:55 -0300

    dashboard updated - done

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -22,7 +22,7 @@ class SessionController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to root_url
   end
 
 end
