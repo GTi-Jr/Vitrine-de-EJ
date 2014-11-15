790cec68306ae59997e540c8b656294754805e94
Gui Carneiro <gui.carneiro15@gmail.com>
2014-11-14 22:00:53 -0300

    subject for emails changed

--- app/mailers/adm_notifier.rb
+++ app/mailers/adm_notifier.rb
@@ -4,6 +4,6 @@ class AdmNotifier < ActionMailer::Base
   def send_feedback(message)
     @message = message
     mail( :to => ADM_EMAIL,
-    :subject => 'Feedback' )
+    :subject => 'Feedback - Vitrine de EJ' )
   end
 end
--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -4,13 +4,13 @@ class UserNotifier < ActionMailer::Base
   def send_signup_email(user)
     @user = user
     mail( :to => @user.email,
-    :subject => 'Thanks for signing up for our amazing app' )
+    :subject => 'Obrigado por se cadastrar - Vitrine de EJ' )
   end
 
   def send_recover_email(user)
     @user = user
     mail( :to => @user.email,
-    :subject => 'Thanks for signing up for our amazing app' )
+    :subject => 'Recupere a sua senha - Vitrine de EJ' )
   end 
 
   def send_message(message)
@@ -19,6 +19,6 @@ class UserNotifier < ActionMailer::Base
     @user = User.find(@je.user_id)
 
     mail( :to => @user.email,
-    :subject => 'Menssagem' )
+    :subject => 'Menssagem - Vitrine de EJ' )
   end
 end
