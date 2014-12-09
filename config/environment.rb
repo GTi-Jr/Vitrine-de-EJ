8f8ba117477700078b6fc9a9b0cc537a4d449643
Gui Carneiro <gui.carneiro15@gmail.com>
2014-12-09 01:50:46 -0300

    initiating API changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,22 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => 'app31876307@heroku.com',
-  :password       => 'swtuqhno',
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
-
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider                         => 'Google',
-    :google_storage_access_key_id     => 'GOOG4PI4RCOWNTIOFSJ4',
-    :google_storage_secret_access_key => 'z55QnnFzJsPYWiiXrYWOsAxznLiHXxLgJjh/8+YL'
-  }
-  config.fog_directory = 'vitrineej'
-end
